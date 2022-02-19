7fc408badf6d8313d7670a16306ad30ca0727666
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-19 12:06:28 +0530

    Add a section that will output Neither if  is neither red nor green

--- src/index.php
+++ src/index.php
@@ -132,6 +132,17 @@ case
     echo "Welcome";
     break;
 }//Create a switch statement that will output "Hello" if $color is "red", and "welcome" if $color is "green".
-
+switch ($color) {
+    case "red":
+      echo "Hello";
+      break;
+    case "green":
+      echo "Welcome";
+      break;
+    
+  default:
+  
+      echo "Neither";
+  } //Add a section that will output "Neither" if $color is neither "red" nor "green"
 ?>
  
