e30ba7195b43bd533adcb270243a9621fb10d293
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-19 11:31:23 +0530

    Statements in PHP have to end with a special character, which one?

--- src/index.php
+++ src/index.php
@@ -6,8 +6,23 @@ echo
 <?php
 
 echo "This is PHP";
-?> #Write the correct opening tag and close tag for PHP scripts.
+?> 
+#Write the correct opening tag and close tag for PHP scripts.
 
 
-//
- This is a single-line comment //ingle-line comments in PHP can be written using two different prefixes, write one of them.
+<?echo 
+
+//This is a single-line comment //ingle-line comments in PHP can be written using two different prefixes, write one of them.
+
+
+
+/*
+ This is a
+multi-line
+comment
+*/ 
+echo "Hello World"
+;
+
+?>
+
