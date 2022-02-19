b4ee0e402c6715a87c6e5af8d19a7abf1d3e7c35
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-19 12:03:24 +0530

    Output 1 if  is equal to , print 2 if  is greater than , otherwise output 3.

--- src/index.php
+++ src/index.php
@@ -103,5 +103,20 @@ else
   echo "No";
 }//Output "Yes" if $a is equal to $b, otherwise output "No".
 
+$a = 50;
+$b = 10;
+if
+ ($a == $b) {
+  echo "1";
+} 
+elseif
+ ($a > $b) {
+  echo "2";
+} 
+else
+ {
+  echo "3";
+}//Output "1" if $a is equal to $b, print "2" if $a is greater than $b, otherwise output "3".
+
 ?>
  
