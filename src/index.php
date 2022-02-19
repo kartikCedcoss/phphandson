c592a7c58a58ec4bb04c80ae6d251dbc6bdb2138
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-19 11:27:39 +0530

    ingle-line comments in PHP can be written using two different prefixes, write one of them.

--- src/index.php
+++ src/index.php
@@ -7,3 +7,7 @@ echo
 
 echo "This is PHP";
 ?> #Write the correct opening tag and close tag for PHP scripts.
+
+
+//
+ This is a single-line comment //ingle-line comments in PHP can be written using two different prefixes, write one of them.
