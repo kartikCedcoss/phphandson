b51b78bfca93d03ee249c31debbb64841136426d
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-19 12:00:56 +0530

    Output 'Hello World' if  is NOT equal to .

--- src/index.php
+++ src/index.php
@@ -80,5 +80,18 @@ $b)
   echo "Hello World";
 } // Output "Hello World" if $a is greater than $b.
 
+$a = 50;
+$b = 10;
+if
+ 
+($a
+ 
+!=
+ 
+$b)
+ {
+  echo "Hello World";
+}//Output "Hello World" if $a is NOT equal to $b.
+
 ?>
  
